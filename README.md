f91c86ef691a1a0e6ca2c6a1e2940d2f8543105a
Stefan Andrei <andreistefanciprian@gmail.com>
2023-02-24 15:34:38 +1030

    add doc

--- README.md
+++ README.md
@@ -1,12 +1,42 @@
-# k8s_informer
+# Description
 
-Build a k8s informer that monitors k8s EVents and deletes Pods that have matching Event Reason and Event Message.
+This program written in Go monitors Kubernetes events, identifies Pods that meet specific Event Reason and Event Message and then deletes them. 
+
+It uses the Kubernetes API and client libraries to manage a shared informer, which is used to monitor events. 
+
+The program uses a command-line option to enable dry run mode and log actions without changing the cluster.
+
+## CLI params
+
+#### `--event-reason`
+- sets the eventReason variable in the program
+- default value: 'FailedCreatePodSandBox'
+
+#### `--event-message`
+- sets the eventMessage variable in the program
+- default value: 'container veth name provided (eth0) already exists'
+
+#### `--dry-run`
+- enable dry run mode and log actions without changing the cluster
+- this option is disabled (set to false) by default
+
+## Run and test on local machine
 
 ```
-# run script
-go run main.go --event-reason BackOff --event-message="Back-off pulling image"
+# install dependencies
+go mod init
+go mod tidy
 
-# create failing Pods
+# compile source code into executable binary
+go build -o pod-deleter
+
+# run program in dry run mode
+./pod-deleter --dry-run
+
+# run program
+./pod-deleter --event-reason BackOff --event-message="Back-off pulling image"
+
+# generate failing pods to test if program is deleting the Pods
 for i in {1..5};do kubectl run foo-$i --image nginx:bad; done
 
 # delete pods
--- main.go
+++ main.go
@@ -8,77 +8,85 @@ import (
 	"strings"
 	"time"
 
+	// Kubernetes API types
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	// Kubernetes client libraries
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-var (
-	eventMessage string
-	eventReason  string
-	dryRunMode   bool
-	ctx          = context.TODO()
-)
-
+// Struct to represent a Pod to be deleted
 type PodToDelete struct {
-	Namespace string
-	Name      string
+	Namespace string // namespace of the Pod
+	Name      string // name of the Pod
 }
 
-var podDeleteQueue = make(chan PodToDelete)
+var (
+	eventMessage   string                   // message used to identify events to be processed
+	eventReason    string                   // reason used to identify events to be processed
+	dryRunMode     bool                     // if true, do not actually delete the Pods
+	ctx            = context.TODO()         // context used to make Kubernetes API calls
+	podDeleteQueue = make(chan PodToDelete) // channel to hold Pods to be deleted
+)
 
 func main() {
-	// define and parse cli params
+	// Define and parse command-line flags
 	flag.BoolVar(&dryRunMode, "dry-run", false, "enable dry run mode (no changes are made, only logged)")
-	flag.StringVar(&eventReason, "event-reason", "FailedCreatePodSandBox", "restart Pods that match Event Reason")
+	flag.StringVar(&eventReason, "event-reason", "FailedCreatePodSandBox", "specify Event Reason to match")
 	flag.StringVar(
 		&eventMessage,
 		"event-message",
 		"container veth name provided (eth0) already exists",
-		"number of seconds between iterations",
+		"specify Event Message to match",
 	)
 
+	// Get the path to the kubeconfig file
 	defaultKubeconfig := os.Getenv(clientcmd.RecommendedConfigPathEnvVar)
 	if len(defaultKubeconfig) == 0 {
 		defaultKubeconfig = clientcmd.RecommendedHomeFile
 	}
 
+	// Parse the kubeconfig file path from the command-line
 	kubeconfig := flag.String(clientcmd.RecommendedConfigPathFlag,
 		defaultKubeconfig, "absolute path to the kubeconfig file")
 	flag.Parse()
 
+	// Create a Kubernetes client config from the kubeconfig file
 	rc, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	// create a client set from config
+	// Create a client set from the config
 	clientSet, err := kubernetes.NewForConfig(rc)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	// create a new instance of sharedInformerFactory for all namespaces
+	// Create a new shared informer factory for all namespaces
 	informerFactory := informers.NewSharedInformerFactory(clientSet, time.Minute*1)
 
-	// using this factory create an informer for k8s resources
+	// Use the factory to create an informer for Kubernetes events
 	k8sObjectInformer := informerFactory.Core().V1().Events()
 
-	// adds an event handler to the shared informer
+	// Add an event handler to the shared informer
 	k8sObjectInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc:    onAdd,
 		UpdateFunc: onUpdate,
 		DeleteFunc: nil,
 	})
 
+	// Start a goroutine to handle the deletion of Pods from the deletion queue
 	go func() {
 		for {
 			select {
 			case pd := <-podDeleteQueue:
 				if !dryRunMode {
+					// Delete the Pod from the Kubernetes API
 					err := clientSet.CoreV1().Pods(pd.Namespace).Delete(ctx, pd.Name, metav1.DeleteOptions{})
 					if err != nil {
 						log.Printf("Failed to delete Pod %s/%s: %v\n", pd.Namespace, pd.Name, err)
@@ -95,21 +103,22 @@ func main() {
 	stopCh := make(chan struct{})
 	defer close(stopCh)
 
-	// starts the shared informers that have been created by the factory
+	// Start the shared informers that have been created by the factory
 	informerFactory.Start(stopCh)
 
-	// wait for the initial synchronization of the local cache
+	// Wait for the initial synchronization of the local cache
 	if !cache.WaitForCacheSync(stopCh, k8sObjectInformer.Informer().HasSynced) {
 		panic("failed to sync")
 	}
 
-	// causes the goroutine to block (hit CTRL+C to exit)
+	// Causes the goroutine to block (hit CTRL+C to exit)
 	select {}
-
 }
 
+// Handler function to process new events
 func onAdd(newObj interface{}) {
 	item := newObj.(*corev1.Event)
+	// Check if the event reason and message match the configured values
 	if item.Reason == eventReason && strings.Contains(item.Message, eventMessage) {
 		log.Printf(
 			"ADD ResVer(%s): Sending Pod %s/%s to the podDeleteQueue.\n",
@@ -117,7 +126,7 @@ func onAdd(newObj interface{}) {
 			item.InvolvedObject.Namespace,
 			item.InvolvedObject.Name,
 		)
-		// put Pod in deletion queue
+		// Put the Pod in the deletion queue
 		podDeleteQueue <- PodToDelete{
 			Namespace: item.InvolvedObject.Namespace,
 			Name:      item.InvolvedObject.Name,
@@ -125,17 +134,18 @@ func onAdd(newObj interface{}) {
 	}
 }
 
+// Handler function to process updated events
 func onUpdate(old, new interface{}) {
 	// oldEvent := old.(*corev1.Event)
 	item := new.(*corev1.Event)
-
+	// Check if the event reason and message match the configured values
 	if item.Reason == eventReason && strings.Contains(item.Message, eventMessage) {
 		log.Printf(
 			"UPDATED ResVer(%s): Sending Pod %s/%s to the podDeleteQueue.\n",
 			item.GetResourceVersion(),
 			item.InvolvedObject.Namespace, item.InvolvedObject.Name,
 		)
-		// put Pod in deletion queue
+		// Put the Pod in the deletion queue
 		podDeleteQueue <- PodToDelete{
 			Namespace: item.InvolvedObject.Namespace,
 			Name:      item.InvolvedObject.Name,
